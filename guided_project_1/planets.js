308108ead5f827ba93db09b8f6005febc9a9eb63
Alice Smith <alice@smith.com>
2024-01-23 22:29:37 -0500

    Added planets page. In planets.js, changed filmsDiv and charactersDiv to filmsUl and charactersUl.

---
+++ guided_project_1/planets.js
@@ -0,0 +1,69 @@
+let nameH1;
+let climateSpan;
+let terrainSpan;
+let gravitySpan;
+let populationSpan;
+let filmsUl;
+let charactersUl;
+const baseUrl = `https://swapi2.azurewebsites.net/api`;
+
+// Runs on page load
+addEventListener('DOMContentLoaded', () => {
+  nameH1 = document.querySelector('h1#name');
+  climateSpan = document.querySelector('span#climate');
+  terrainSpan = document.querySelector('span#terrain');
+  gravitySpan = document.querySelector('span#gravity');
+  populationSpan = document.querySelector('span#population');
+  filmsUl = document.querySelector('#films>ul');
+  charactersUl = document.querySelector("#characters>ul");
+  const sp = new URLSearchParams(window.location.search)
+  const id = sp.get('id')
+  getPlanet(id)
+});
+
+async function getPlanet(id) {
+  let planet;
+  try {
+    planet = await fetchPlanet(id)
+    planet.characters = await fetchCharacters(planet)
+    planet.films = await fetchFilms(planet)
+  }
+  catch (ex) {
+    console.error(`Error reading planet ${id} data.`, ex.message);
+  }
+  renderCharacter(planet);
+
+}
+async function fetchPlanet(id) {
+  let planetUrl = `${baseUrl}/planets/${id}`;
+  return await fetch(planetUrl)
+    .then(res => res.json())
+}
+
+async function fetchCharacters(planet) {
+  const url = `${baseUrl}/planets/${planet?.id}/characters`;
+  const characters = await fetch(url)
+    .then(res => res.json())
+  return characters;
+}
+
+async function fetchFilms(planet) {
+  const url = `${baseUrl}/planets/${planet?.id}/films`;
+  const films = await fetch(url)
+    .then(res => res.json())
+  return films;
+}
+
+const renderCharacter = planet => {
+  document.title = `SWAPI - ${planet?.name}`;  // Just to make the browser tab say their name
+  nameH1.textContent = planet?.name;
+  climateSpan.textContent = planet?.climate;
+  terrainSpan.textContent = planet?.terrain;
+  gravitySpan.textContent = planet?.gravity;
+  populationSpan.textContent = planet?.population;
+  //homeworldSpan.innerHTML = `<a href="/planet.html?id=${character?.homeworld.id}">${character?.homeworld.name}</a>`;
+  const charactersLis = planet?.characters?.map(character => `<li><a href="/character.html?id=${character.id}">${character.name}</li>`)
+  charactersUl.innerHTML = charactersLis.join("");
+  const filmsLis = planet?.films?.map(film => `<li><a href="/film.html?id=${film.id}">${film.title}</li>`)
+  filmsUl.innerHTML = filmsLis.join("");
+}
